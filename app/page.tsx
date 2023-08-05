d05e8cefa8f711d5d40c409f05c6b4a54c1aff05
Timo Heman <timoheman16@gmail.com>
2023-08-05 21:11:46 +0100

    added portfolio copu

--- app/page.tsx
+++ app/page.tsx
@@ -12,16 +12,16 @@ import { Typewriter } from "react-simple-typewriter";
 export default function Home() {
 	const routes = [
 		{
-			href: "#home",
-			label: "Home",
+			href: "https://twitter.com/timo__tech",
+			label: "Twitter (X)",
 		},
 		{
-			href: "#experience",
-			label: "Experience",
+			href: "https://www.linkedin.com/in/timo-heman/",
+			label: "LinkedIn",
 		},
 		{
-			href: "#projects",
-			label: "Projects",
+			href: "https://github.com/timo-tech19",
+			label: "Github",
 		},
 	];
 
@@ -30,22 +30,38 @@ export default function Home() {
 			imageUrl: "/appchat.png",
 			name: "App Chat",
 			description:
-				"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque dignissimos sed similique, nobis numquam iste itaque ducimus magnam harum.",
+				"A social media type application which helps people kickoff conversations by postings topics and having people reply directly to their DMs.",
 			github: "",
+			stack: ["React", "Firebase", "TailWind CSS"],
 		},
 		{
 			imageUrl: "/ecommerce.png",
 			name: "Ecommerce App",
 			description:
-				"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque dignissimos sed similique, nobis numquam iste itaque ducimus magnam harum.",
+				"An ecommerce platform with a front end (store) and an admin area from managing your store.",
 			github: "",
+			stack: [
+				"Nextjs",
+				"Zustand",
+				"Tailwind CSS",
+				"Prisma",
+				"Cloudinary",
+				"Clerk Auth",
+			],
 		},
 		{
 			imageUrl: "/music.png",
 			name: "Music App",
 			description:
-				"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque dignissimos sed similique, nobis numquam iste itaque ducimus magnam harum.",
+				"Beautiful music application where you can upload your favourite songs and listen from anywhere.",
 			github: "",
+			stack: [
+				"Nextjs",
+				"Supabase",
+				"Tailwind CSS",
+				"Zustand",
+				"React Hook Form",
+			],
 		},
 	];
 
@@ -60,7 +76,13 @@ export default function Home() {
 						<ul className='flex items-center gap-x-8 font-semibold uppercase'>
 							{routes.map((route) => (
 								<li key={route.href}>
-									<Link href={route.href}>{route.label}</Link>
+									<Link
+										href={route.href}
+										target='_blank'
+										className='hover:underline'
+									>
+										{route.label}
+									</Link>
 								</li>
 							))}
 						</ul>
@@ -68,108 +90,151 @@ export default function Home() {
 				</div>
 			</Container>
 			<main>
-				<section>
-					<Container>
-						<div className='flex items-center justify-around my-16'>
-							<div className='w-1/2'>
-								<h1 className='text-7xl font-extrabold uppercase tracking-wider'>
-									Timo{" "}
-									<span className='font-semibold'>
-										<RoughNotation type='highlight' show color='#bbf7d0'>
-											Heman
+				<RoughNotationGroup show>
+					<section>
+						<Container>
+							<div className='flex flex-col-reverse px-4 lg:px-0 lg:flex-row items-center justify-around my-8 lg:my-16'>
+								<div className='w-full lg:w-1/2 text-center lg:text-left'>
+									<h1 className='text-5xl lg:text-7xl font-extrabold uppercase tracking-wider'>
+										Timo{" "}
+										<span className='font-semibold'>
+											<RoughNotation type='highlight' color='#bbf7d0' order='1'>
+												Heman
+											</RoughNotation>
+										</span>
+									</h1>
+									<hr className='mx-auto lg:ml-4 w-16 my-4 border-green-500 border-4' />
+									<h3 className='mb-4 text-3xl lg:text-5xl'>
+										I am a{" "}
+										<span className='text-stone-700 font-semibold'>
+											<Typewriter
+												words={[
+													"Software Developer",
+													"Fast-paced Learner",
+													"Problem Solver",
+													"great fit for the job!",
+												]}
+												loop={0}
+												cursor
+												cursorStyle='<'
+											/>
+										</span>
+									</h3>
+									<p className='mb-2 leading-7 text-md lg:text-lg'>
+										Hello there, I&apos;m Timo from Cameroon. I&apos;ve been
+										working professionally as a{" "}
+										<RoughNotation type='highlight' color='#a5b4fc' order='2'>
+											software developer for over a year.
+										</RoughNotation>{" "}
+										I started learning how to code about five years ago, two
+										years before completing high school. Being{" "}
+										<RoughNotation type='highlight' color='#bbf7d0' order='3'>
+											{" "}
+											self-taught
+										</RoughNotation>{" "}
+										has given me control over my learning process. It has also
+										given me the opportunity to{" "}
+										<RoughNotation type='highlight' color='#bae6fd' order='4'>
+											{" "}
+											learn skills and technologies much faster
+										</RoughNotation>{" "}
+										- an invaluable skill when{" "}
+										<RoughNotation type='highlight' color='#fde68a' order='5'>
+											working in startup environments.
 										</RoughNotation>
-									</span>
-								</h1>
-								<hr className='ml-4 w-16 my-4 border-green-500 border-4' />
-								<h3 className='mb-4 text-5xl'>
-									I am a{" "}
-									<span className='text-stone-700 font-semibold'>
-										<Typewriter
-											words={[
-												"Software Developer",
-												"Fast-paced Learner",
-												"Problem Solver",
-												"great fit for the job!",
-											]}
-											loop={0}
-											cursor
-											cursorStyle='<'
+									</p>
+									<p className='leading-7 text-md lg:text-lg'>
+										One of the reasons why I learned programming is because I
+										wanted to work on cutting-edge projects that{" "}
+										<RoughNotation type='underline' color='#1e40af' order='6'>
+											solve complex problems
+										</RoughNotation>{" "}
+										in our ever-expanding digital world. At startups, I&apos;ve
+										enjoyed working through problems that had me{" "}
+										<RoughNotation type='box' color='#f97316' order='7'>
+											googling and researching
+										</RoughNotation>{" "}
+										for the best possible solutions. It gets frustrating
+										sometimes, but it&apos;s all smiles when it works. I&apos;m
+										not scared of a challenge. Now I&apos;m looking for new
+										opportunities that will further stretch my abilities.{" "}
+										<RoughNotation type='circle' color='#dc2626' order='8'>
+											Wanna hire me?
+										</RoughNotation>
+									</p>
+									<div className='mt-10'>
+										<Link
+											href='mailto:timoheman16@gmail.com'
+											target='_blank'
+											className='rounded-md bg-amber-300 px-4 py-3 text-lg font-semibold shadow-sm hover:bg-amber-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-300'
+										>
+											Let&apos;s talk <span aria-hidden='true'>→</span>
+										</Link>
+									</div>
+								</div>
+								<div>
+									<div className='rounded-full lg:rounded-xl overflow-hidden drop-shadow-2xl mb-8 lg:mb-0'>
+										<Image
+											className='object-cover object-top max-lg:w-[200px] max-lg:h-[200px]'
+											src='/me.jpg'
+											alt='Timo Heman'
+											width={400}
+											height={400}
 										/>
-									</span>
-								</h3>
-								<p className='mb-2 leading-7 text-lg'>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Consequatur aliquid, a, nesciunt recusandae soluta debitis
-									reprehenderit perspiciatis quia error atque aspernatur iure
-									quam similique. Ab qui quam nesciunt ea non odio repellat
-									corrupti est maiores iusto neque dolores, sed laboriosam.
-								</p>
-								<p className='leading-7 text-lg'>
-									Dolor sit amet consectetur adipisicing elit. Quas sit
-									necessitatibus repellendus fugit nostrum cupiditate minima
-									error tempore esse asperiores, nisi, voluptates soluta iusto
-									officia, nobis optio ipsa facilis{" "}
-									<RoughNotation type='circle' show animate color='#ff0000'>
-										perspiciatis?
-									</RoughNotation>
-								</p>
-								<div className='mt-10'>
-									<Link
-										href='#'
-										className='rounded-md bg-amber-300 px-4 py-3 text-lg font-semibold shadow-sm hover:bg-amber-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-300'
-									>
-										Let&apos;s talk <span aria-hidden='true'>→</span>
-									</Link>
+									</div>
 								</div>
 							</div>
-							<div>
-								<div className='rounded-xl overflow-hidden drop-shadow-2xl'>
-									<Image
-										className='object-cover'
-										src='/me.jpg'
-										alt='Timo Heman'
-										width={400}
-										height={400}
-									/>
-								</div>
+							<div className='flex items-center justify-center mt-16 lg:mt-0'>
+								<RoughNotation
+									customElement='div'
+									type='bracket'
+									brackets={["left", "right"]}
+									order='9'
+									padding={20}
+									color='#1c1917'
+								>
+									<div className='flex items-center justify-center gap-x-8 lg:gap-x-24'>
+										<Image
+											className='max-lg:w-12 max-lg:h-12'
+											src='/react.svg'
+											alt='React'
+											width={60}
+											height={60}
+										/>
+										<Image
+											className='max-lg:w-12 max-lg:h-12'
+											src='/nextjs.svg'
+											alt='Next'
+											width={60}
+											height={60}
+										/>
+										<Image
+											className='max-lg:w-12 max-lg:h-12'
+											src='/tailwind.svg'
+											alt='Tailwind Css'
+											width={60}
+											height={60}
+										/>
+										<Image
+											className='max-lg:w-12 max-lg:h-12'
+											src='/typescript.svg'
+											alt='TypeScript'
+											width={60}
+											height={60}
+										/>
+										<Image
+											className='max-lg:w-12 max-lg:h-12'
+											src='/javascript.svg'
+											alt='Javascipt'
+											width={60}
+											height={60}
+										/>
+									</div>
+								</RoughNotation>
 							</div>
-						</div>
-						<div className='flex items-center justify-center'>
-							<RoughNotation
-								customElement='div'
-								type='bracket'
-								brackets={["left", "right"]}
-								show
-								padding={20}
-								color='#dc2626'
-							>
-								<div className='flex items-center justify-center gap-x-24'>
-									<Image src='/react.svg' alt='React' width={60} height={60} />
-									<Image src='/nextjs.svg' alt='Next' width={60} height={60} />
-									<Image
-										src='/tailwind.svg'
-										alt='Tailwind Css'
-										width={60}
-										height={60}
-									/>
-									<Image
-										src='/typescript.svg'
-										alt='TypeScript'
-										width={60}
-										height={60}
-									/>
-									<Image
-										src='/javascript.svg'
-										alt='Javascipt'
-										width={60}
-										height={60}
-									/>
-								</div>
-							</RoughNotation>
-						</div>
-					</Container>
-				</section>
-
+						</Container>
+					</section>
+				</RoughNotationGroup>
 				<section className='mt-16 py-4 bg-stone-50'>
 					<Container>
 						<Heading title='Previously worked at' />
@@ -197,7 +262,7 @@ export default function Home() {
 									key={project.name}
 									index={index + 1}
 									{...project}
-									className={index === 1 ? "flex-row-reverse" : ""}
+									className={index === 1 ? "lg:flex-row-reverse" : ""}
 									video='/appchat-mvp.mp4'
 								/>
 							))}
@@ -211,13 +276,35 @@ export default function Home() {
 							<h2 className='text-4xl font-semibold tracking-wide'>
 								Wanna Hire Me?
 							</h2>
-							<p className='text-xl mt-2'>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
-								quibusdam dolore quaerat.
+							<p className='text-lg mt-2 max-w-2xl mx-auto'>
+								Thanks for taking the time to check out my portfolio. If you
+								found my skills valuable to you, I&apos;d love to have a
+								conversation. You can reach out to me on{" "}
+								<Link
+									href='https://twitter.com/timo__tech'
+									target='_blank'
+									className='underline text-green-500'
+								>
+									Twitter
+								</Link>{" "}
+								or{" "}
+								<Link
+									href='https://www.linkedin.com/in/timo-heman/'
+									target='_blank'
+									className='underline text-green-500'
+								>
+									LinkedIn
+								</Link>{" "}
+								. You can also click the button below to send me an email.
+								Looking forward to hearing from you!
 							</p>
-							<button className=' mt-4 uppercase bg-amber-300 px-8 py-3 text-xl font-semibold shadow-lg hover:bg-amber-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-300 transition'>
+							<Link
+								href='mailto:timoheman16@gmail.com'
+								target='_blank'
+								className='inline-block mt-4 uppercase bg-amber-300 px-8 py-3 text-xl font-semibold shadow-lg hover:bg-amber-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-300 transition'
+							>
 								Let&apos;s Talk!
-							</button>
+							</Link>
 						</div>
 					</Container>
 				</footer>
--- components/project.tsx
+++ components/project.tsx
@@ -10,6 +10,7 @@ interface ProjectProps {
 	github: string;
 	className?: string;
 	video: string;
+	stack: string[];
 }
 
 const Project: React.FC<ProjectProps> = ({
@@ -20,11 +21,14 @@ const Project: React.FC<ProjectProps> = ({
 	github,
 	className,
 	video,
+	stack,
 }) => {
 	const videoModal = useVideoModal();
 
 	return (
-		<div className={`flex justify-around items-center mt-20 ${className}`}>
+		<div
+			className={`flex flex-col lg:flex-row lg:justify-around items-center mt-20 ${className}`}
+		>
 			<div className='flex'>
 				<span className='text-5xl font-bold border-r-4 pr-2 border-r-green-500 self-start'>
 					0{index}
@@ -33,10 +37,17 @@ const Project: React.FC<ProjectProps> = ({
 					<Image src={imageUrl} alt={name} width={400} height={400} />
 				</div>
 			</div>
-			<div className='w-5/12'>
+			<div className='lg:w-1/3 text-center lg:text-left w-3/4 mt-8 lg:mt-0'>
 				<h3 className='text-3xl font-semibold'>{name}</h3>
+				<ul className='mt-2 flex gap-x-3 items-center justify-center lg:justify-stretch flex-wrap gap-y-4'>
+					{stack.map((item) => (
+						<li key={item} className='bg-stone-200 py-1 px-2 rounded-md'>
+							{item}
+						</li>
+					))}
+				</ul>
 				<p className='mt-4'>{description}</p>
-				<div className='mt-4 flex gap-x-2'>
+				<div className='mt-4 flex justify-center lg:justify-stretch gap-x-2'>
 					<Link
 						href={github}
 						className='rounded-md border-2 border-amber-300 px-3 py-1.5 text-lg font-semibold shadow-sm hover:bg-amber-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-300 transition'
